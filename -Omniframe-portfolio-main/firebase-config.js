1ba729e04dfc88e85bf97549e3be9a2bea8a004d
Akhilkanta05 <akhilsanju2005@gmail.com>
2026-02-01 17:37:36 +0530

    Update firebase-config.js

--- -Omniframe-portfolio-main/firebase-config.js
+++ -Omniframe-portfolio-main/firebase-config.js
@@ -1,18 +1,10 @@
-// --- Firebase Configuration ---
-// Configured by User
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyA7vIrojfHZhz52gplHCaKbE_xEQ5yGSk4",
-    authDomain: "omniframes-d7612.firebaseapp.com",
-    projectId: "omniframes-d7612",
-    storageBucket: "omniframes-d7612.firebasestorage.app",
-    messagingSenderId: "964170903282",
-    appId: "1:964170903282:web:dec31a5d6681c62eb6ea6b",
-    measurementId: "G-91R21WQY7V"
+  apiKey: "AIzaSyA7vIrojfHZhz52gplHCaKbE_xEQ5yGSk4",
+  authDomain: "omniframes-d7612.firebaseapp.com",
+  projectId: "omniframes-d7612",
+  storageBucket: "omniframes-d7612.firebasestorage.app",
+  messagingSenderId: "964170903282",
+  appId: "1:964170903282:web:dec31a5d6681c62eb6ea6b",
+  measurementId: "G-91R21WQY7V"
 };
-
-// Initialize Firebase (Compat version for HTML script tags)
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const db = firebase.firestore();
-const storage = firebase.storage();
-const analytics = firebase.analytics();
